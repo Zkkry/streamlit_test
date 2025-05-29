6259540bcfbed948f528ee6d5fb8188aabb9794c
Zkkry <zikryy.kl@gmail.com>
2025-05-29 19:38:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Set the app title 
-st.title('HELOO !!') 
+st.title('SEA TIDE FORECAST !!') 
 
 # Add a welcome message 
 st.write('Welcome to my Streamlit app BOSS!') 
