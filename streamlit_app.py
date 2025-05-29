34dabfa64b3666c7b74fc8b477119e6c6e358e47
Zkkry <zikryy.kl@gmail.com>
2025-05-29 20:11:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("🌤️ Weather App")
 
 API_KEY = "cb367eaa6af232c7c8bca6a77714c7ca" 
 
-city = st.text_input("Enter city name:", "Penang")
+city = st.text_input("Enter city name:", "Pahang")
 
 if city:
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
