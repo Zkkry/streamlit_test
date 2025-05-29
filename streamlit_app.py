99621f899248cc4e12d9c6fd4aa216784dc5c734
Zkkry <zikryy.kl@gmail.com>
2025-05-29 20:10:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import requests
 
 st.title("🌤️ Weather App")
 
-city = st.text_input("Enter a city name:", "Penang")
-API_KEY = "4a801bd757d1677a8c5a3af8ec080464"
+API_KEY = "cb367eaa6af232c7c8bca6a77714c7ca" 
+
+city = st.text_input("Enter city name:", "Penang")
 
 if city:
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
@@ -12,9 +13,10 @@ if city:
 
     if response.status_code == 200:
         data = response.json()
-        st.write(f"Weather in {city}")
-        st.write("Temperature:", data["main"]["temp"], "°C")
-        st.write("Description:", data["weather"][0]["description"])
+        st.subheader(f"Weather in {city}")
+        st.write("🌡️ Temp:", data["main"]["temp"], "°C")
+        st.write("💧 Humidity:", data["main"]["humidity"], "%")
+        st.write("🌬️ Wind:", data["wind"]["speed"], "m/s")
     else:
         st.error("City not found or API error.")
-        st.code(response.text)  # This shows the actual API error
+        st.code(response.text)
